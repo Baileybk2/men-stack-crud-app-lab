c3d869e102d6468132085f58a719da378ca7f96d
BaileyBeck <baileybk2@gmail.com>
2024-06-25 20:30:59 -0400

    completed lab

--- models/birds.js
+++ models/birds.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 
 const birdSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  description: { type: String, required: true },
-  image: String,
 });
 
 const Birds = mongoose.model("Birds", birdSchema);
